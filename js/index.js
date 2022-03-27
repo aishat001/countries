71077e56c67a5a09383b9a28a03d0f090eb7055f
aishat001 <waheedaishat@gmail.com>
2022-03-27 16:49:11 +0100

    fix darkmode

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const search = document.getElementById('search')
 const countryModal = document.getElementById('countryModal')
 const select = document.getElementById('select')
 const toggleDarkMode = document.getElementById('toggle')
+const myHeader = document.getElementById('header')
+// const searchContainer = document.getElementById('searchContainer')
+// const selectContainer = document.getElementById('selectContainer')
 
 
 let regionValue = "";
@@ -19,7 +22,6 @@ const fetchCountries = async () => {
     console.log(countries);
 };
 
-
 const border = (item) => {
     if (!item) {
         return " None"
@@ -27,20 +29,36 @@ const border = (item) => {
     item.map(border => border)
 }
 
+toggleDarkMode.addEventListener('click', () => {
+
+    document.body.classList.toggle('background-dark')
+    countryModal.classList.toggle('background-dark')
+    myHeader.classList.toggle('darkmodeElement')
+    toggleDarkMode.classList.toggle('darkmodeElement')
+    // search.classList.toggle('darkmodeElement')
+    // select.classList.toggle('darkmodeElement')
+    // searchContainer.classList.toggle('darkmodeElement')
+    // selectContainer.classList.toggle('darkmodeElement')
+})
+
 const showCountryDetails = (item) => {
     countryModal.classList.remove('hide')
     countryModal.classList.toggle('show')
     countryModal.innerHTML = `
-    <button class="backBtn" id="backBtn">back</button>
+    <button class="backBtn border-0 bg-white py-1 px-2 rounded" id="backBtn">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+</svg>
+Back</button>
 
 
-    <div class="d-flex flex-row flex-wrap justify-content-between card border-0 py-5">
+    <div class="d-flex flex-row flex-wrap justify-content-between  border-0 py-3 countryModalDetail lightmodeElement">
         <div class="col-10 col-md-5 pr-2">
         <img src ='${item.flag}' alt='country' width="100%" height="250px" class="img-fluid detailImg"/>
 
         </div>
 
-        <div class="details card-body col-10 col-md-4 px-2 py-0"> 
+        <div class="details card-body col-10 col-md-4 px-2 py-0 lightmodeElement"> 
             <p class='card-title mb-5 fs-5'><strong>${item.name}</strong></p>
 
             <div class="d-flex row justify-content-start">
@@ -73,8 +91,28 @@ const showCountryDetails = (item) => {
 
         })
     }
-}
 
+    const Cmodal = document.querySelector('.countryModalDetail')
+    const Cdetails = document.querySelector('.details')
+
+    if (Cmodal && countryModal.classList.contains('background-dark')) {
+        Cmodal.classList.toggle('background-dark')
+        Cdetails.classList.toggle('background-dark')
+
+    } else if (Cmodal && countryModal.classList.contains('lightmodeElement')) {
+        Cmodal.classList.toggle('lightmodeElement')
+        Cdetails.classList.toggle('lightmodeElement')
+    }
+
+
+    toggleDarkMode.addEventListener('click', () => {
+
+        if (Cmodal) {
+            Cmodal.classList.toggle('background-dark')
+            Cdetails.classList.toggle('background-dark')
+        }
+    })
+}
 
 const showCountries = async () => {
     results.innerHTML = '';
@@ -89,7 +127,7 @@ const showCountries = async () => {
         .forEach(country => {
 
             const li = document.createElement('div');
-            li.classList.add('country-item', 'card', 'mb-5', 'mx-3', 'rounded-3');
+            li.classList.add('country-item', 'card', 'mb-5', 'mx-3', 'rounded-3', 'shadow-lg');
             li.style.width = "20rem"
 
             const country_flag = document.createElement('img');
@@ -98,7 +136,7 @@ const showCountries = async () => {
 
             const country_name = document.createElement('h4');
             country_name.innerText = country.name;
-            country_name.classList.add('card-title', 'px-3');
+            country_name.classList.add('card-title', 'px-3', 'mt-2');
 
             const country_info = document.createElement('div');
             country_info.classList.add('card-body');
@@ -117,8 +155,6 @@ const showCountries = async () => {
             country_population.innerHTML = `<strong> Population : </strong> ${numberWithCommas(country.population)}`;
             country_population.classList.add('country-population');
 
-
-
             country_info.appendChild(country_population);
             country_info.appendChild(country_region);
             country_info.appendChild(country_capital);
@@ -130,13 +166,15 @@ const showCountries = async () => {
             ul.appendChild(li);
 
             li.addEventListener('click', () => {
-                  showCountryDetails(country)
+                showCountryDetails(country)
+
             })
 
             toggleDarkMode.addEventListener('click', () => {
                 document.body.classList.toggle('background-dark')
-                    li.classList.toggle('background-dark')
-                
+                li.classList.toggle('darkmodeElement')
+
+
             })
         })
     results.appendChild(ul)
@@ -157,11 +195,8 @@ select.addEventListener('change', e => {
     showCountries()
 })
 
-toggleDarkMode.addEventListener('click', () => {
-    document.body.classList.toggle('background-dark')
-    
-})
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
